6c2de8cb4fcfc4ac5189d0de8effe39fc9db1573
Masao Kitamura <masaok@gmail.com>
2023-01-26 00:45:08 -0800

    Adding test cases

--- src/services/FetchService.ts
+++ src/services/FetchService.ts
@@ -3,6 +3,11 @@ import { Request } from 'express'
 export const fetchRaw = async (req: Request, res) => {
   const { url } = req.params
 
+  //   const url = 'https%3A%2F%2Fexample-files.online-convert.com%2Fdocument%2Ftxt%2Fexample.txt' // example live text file
+
+  //   const url =
+  //     'https%3A%2F%2Fcalendar.google.com%2Fcalendar%2Fical%2F6to0vu3lbn1e3jlfc8acdp5l3s%2540group.calendar.google.com%2Fpublic%2Fbasic.ics' // example live calendar ICS feed
+
   const decoded = decodeURIComponent(url)
 
   const response = await fetch(decoded)
