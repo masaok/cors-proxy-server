9097a72934bd94732905aa1db5f977c2afb71690
Masao Kitamura <masaok@gmail.com>
2023-01-26 02:51:49 -0800

    Back to the hardcoded GCal URL

--- src/services/FetchService.ts
+++ src/services/FetchService.ts
@@ -1,22 +1,22 @@
 import fetch from 'node-fetch'
-import { Request } from 'express'
+import { Request, Response } from 'express'
 
-export const fetchRaw = async (req: Request, res) => {
+export const fetchRaw = async (req: Request, res: Response) => {
   //   const { url } = req.params
 
   //   const url = 'https%3A%2F%2Fexample-files.online-convert.com%2Fdocument%2Ftxt%2Fexample.txt' // example live text file
 
-  //   const url =
-  //     'https%3A%2F%2Fcalendar.google.com%2Fcalendar%2Fical%2F6to0vu3lbn1e3jlfc8acdp5l3s%2540group.calendar.google.com%2Fpublic%2Fbasic.ics' // example live calendar ICS feed
+  const url =
+    'https%3A%2F%2Fcalendar.google.com%2Fcalendar%2Fical%2F6to0vu3lbn1e3jlfc8acdp5l3s%2540group.calendar.google.com%2Fpublic%2Fbasic.ics' // example live calendar ICS feed
 
-  //   const decoded = decodeURIComponent(url)
+  const decoded = decodeURIComponent(url)
 
-  //   const response = await fetch(decoded)
-  //   const body = await response.text()
+  const response = await fetch(decoded)
+  const body = await response.text()
 
-  //   console.log(decoded)
+  console.log(decoded)
 
-  const body = 'hello'
+  //   const body = 'hello'
 
   res.set('Content-Type', 'text/plain')
   res.send(body)
