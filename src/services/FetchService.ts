de3c6d10621ff36759fe2cfc5f2513cdfec79b5a
Masao Kitamura <masaok@gmail.com>
2023-01-26 01:51:39 -0800

    Removing fetchRaw content

--- src/services/FetchService.ts
+++ src/services/FetchService.ts
@@ -6,15 +6,17 @@ export const fetchRaw = async (req: Request, res) => {
 
   //   const url = 'https%3A%2F%2Fexample-files.online-convert.com%2Fdocument%2Ftxt%2Fexample.txt' // example live text file
 
-  const url =
-    'https%3A%2F%2Fcalendar.google.com%2Fcalendar%2Fical%2F6to0vu3lbn1e3jlfc8acdp5l3s%2540group.calendar.google.com%2Fpublic%2Fbasic.ics' // example live calendar ICS feed
+  //   const url =
+  //     'https%3A%2F%2Fcalendar.google.com%2Fcalendar%2Fical%2F6to0vu3lbn1e3jlfc8acdp5l3s%2540group.calendar.google.com%2Fpublic%2Fbasic.ics' // example live calendar ICS feed
 
-  const decoded = decodeURIComponent(url)
+  //   const decoded = decodeURIComponent(url)
 
-  const response = await fetch(decoded)
-  const body = await response.text()
+  //   const response = await fetch(decoded)
+  //   const body = await response.text()
 
-  console.log(decoded)
+  //   console.log(decoded)
+
+  const body = 'hello'
 
   res.set('Content-Type', 'text/plain')
   res.send(body)
